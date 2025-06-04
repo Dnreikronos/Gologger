96c6751804aa68c9049a361553bde0c46946a139
Dnreikronos <dnreikronos2@gmail.com>
2025-06-04 20:17:01 -0300

    feature[parser]: create parser file

---
+++ internal/parser/parser.go
@@ -0,0 +1,46 @@
+// internal/parser/parser.go
+package parser
+
+import (
+	"Gologger/pkg/types"
+	"context"
+	"regexp"
+	"strings"
+	"sync"
+)
+
+var (
+	errorRegex = regexp.MustCompile(`(?i)\bERROR\b`)
+	warnRegex  = regexp.MustCompile(`(?i)\bWARN(?:ING)?\b`)
+)
+
+func StartParser(ctx context.Context, wg *sync.WaitGroup, in <-chan types.LogLine, out chan<- types.ParsedLog) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case logLine := <-in:
+				level := ""
+				switch {
+				case errorRegex.MatchString(logLine.Line):
+					level = "ERROR"
+				case warnRegex.MatchString(logLine.Line):
+					level = "WARN"
+				default:
+					continue // skip non-matching lines
+				}
+
+				out <- types.ParsedLog{
+					Filename:  logLine.Filename,
+					Level:     level,
+					Message:   strings.TrimSpace(logLine.Line),
+					Timestamp: "",
+				}
+			}
+		}
+	}()
+}
+
